2bbca7ddec2d0071946aad10f9b5103ec2c93aac
Jan Schmitz <jan.schmitz@whu.edu>
2026-03-29 21:30:03 +0200

    Update views.py

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,12 +1,11 @@
 from django.contrib import messages
 from django.contrib.auth import logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.views.decorators.http import require_http_methods
-
 from PIL import Image
 
 
